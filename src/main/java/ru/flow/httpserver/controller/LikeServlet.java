95fa9bc77907320527dd95f595061eea554076f2
flow2708 <dibirovamin10@gmail.com>
2025-07-10 23:34:17 +0300

    Update LikeServlet.java

--- src/main/java/ru/flow/httpserver/controller/LikeServlet.java
+++ src/main/java/ru/flow/httpserver/controller/LikeServlet.java
@@ -29,8 +29,7 @@ public class LikeServlet extends HttpServlet {
         String username = currentUser.getUsername();
         int post_id = Integer.parseInt(req.getParameter("post_id"));
         String action = req.getParameter("action");
-
-        PrintWriter out = resp.getWriter();
+        
         boolean success = false;
 
             switch (action) {
